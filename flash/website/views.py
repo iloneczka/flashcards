1ac2e539a9900df3f99b4c03ee55102fdda25c2b
Ilona Pokrywka <ilo@MacBook-Air-Ilona.local>
2023-08-14 16:56:52 +0200

    Edit all_cards.html

--- flash/website/views.py
+++ flash/website/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from website.models import Card
 
 def home(request):
     return render(request, 'home.html', {})
 
 def all_cards(request):
-    return render(request, 'all_cards.html', {})
+    all_cards = Card.objects.all()
+    print(all_cards)  # Dodaj ten wiersz
+    return render(request, 'all_cards.html', {'all_cards': all_cards})
+
 
 def create_new_card(request):
     if request.method == "POST":
@@ -17,5 +22,10 @@ def create_new_card(request):
     # return render(request, 'create_new_card.html', {})
 
 
-def import_cards(request):
-    return render(request, 'import_cards.html', {})
+def export_cards(request):
+    return render(request, 'export_cards.html', {})
+
+
+# class CardListView(ListView):
+#         model = Card
+#         queryset = Card.objects.all().order_by("box", "-date_created")
