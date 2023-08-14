02fc281a951516575e7ff093637fdb60755d966e
Ilona Pokrywka <ilo@MacBook-Air-Ilona.local>
2023-08-14 15:38:49 +0200

    Edit create_new_card.html

--- flash/website/views.py
+++ flash/website/views.py
@@ -7,7 +7,15 @@ def all_cards(request):
     return render(request, 'all_cards.html', {})
 
 def create_new_card(request):
-    return render(request, 'create_new_card.html', {})
+    if request.method == "POST":
+        answer = request.POST['answer']
+        question = request.POST['question']
+        print(f"XXXXX {answer}")
+        return render(request, 'create_new_card.html', {'added': True, 'question': question, 'answer': answer})
+    return render(request, 'create_new_card.html', {'added': False})
+
+    # return render(request, 'create_new_card.html', {})
+
 
 def import_cards(request):
     return render(request, 'import_cards.html', {})
